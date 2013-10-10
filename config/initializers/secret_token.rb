41d9788e3b3ffe89917b144fb2d9ee6470eab768
Matteo Canu <matteo.canu@devise.it>
2013-10-10 12:56:15 +0200

    Git init with shame on treehouse

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '89c29b96d12bd044d4c37fe9dd4d4abbf7a17ea8726dddf285bb1a179c7badc4ae70cba39d59be76d19bfa555b0e844f98ff5431dd3fc25631ae00276ec0e7cb'
