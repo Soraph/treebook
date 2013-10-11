6534c29818c106d513b5983e4e8372f8c61fba4f
Matteo Canu <matteo.canu@devise.it>
2013-10-11 15:08:01 +0200

    Installed devise views, add a user

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Treebook::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # devise conf
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131011101345) do
+ActiveRecord::Schema.define(:version => 20131011130318) do
 
   create_table "statuses", :force => true do |t|
-    t.string   "name"
     t.text     "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
+
+  add_index "statuses", ["user_id"], :name => "index_statuses_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "first_name"
