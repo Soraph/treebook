0c7d3c4c516a9eb539f153a2397098197913e228
Matteo Canu <matteo.canu@devise.it>
2013-10-14 11:22:14 +0200

    Added helper to handle different types of flash messages as CSS classes for the new styling of the flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+    def flash_css_class(type)
+        case type
+        when :alert
+          "alert-warning"
+        when :notice
+          "alert-success"
+        else
+            ""
+        end
+    end
+
 end
