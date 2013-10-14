64336176e3e3199e41ef91819e0901d6ca7518a7
Matteo Canu <matteo.canu@devise.it>
2013-10-14 15:52:06 +0200

    Added status validations and tests

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,6 +1,6 @@
 class StatusesController < ApplicationController
 
-  before_filter :authenticate_user!, only: [:new]
+  before_filter :authenticate_user!, only: [:new, :create, :edit, :update]
 
   # GET /statuses
   # GET /statuses.json
--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -23,7 +23,14 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should create status" do
+  test "should be logged in to post a status" do
+    post :create, status: { content: "Hello" }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should create status when logged in" do
+    sign_in users(:profiler)
     assert_difference('Status.count') do
       post :create, status: { content: @status.content }
     end
@@ -36,12 +43,26 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should be logged in to edit a status" do
+    get :edit, id: @status
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should get edit if logged in" do
+    sign_in users(:profiler)
     get :edit, id: @status
     assert_response :success
   end
 
-  test "should update status" do
+  test "should be logged in to update a status" do
+    put :update, id: @status, status: { content: @status.content }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should update status if logged in" do
+    sign_in users(:profiler)
     put :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
   end
