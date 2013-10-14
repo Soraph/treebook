9d622a41be2580e790a908a0ac6e650ecf187d2a
Matteo Canu <matteo.canu@devise.it>
2013-10-14 17:54:58 +0200

    Added tests for the new route and profile controller and page

--- test/integration/custom_routes_test.rb
+++ test/integration/custom_routes_test.rb
@@ -16,4 +16,9 @@ class CustomRoutesTest < ActionDispatch::IntegrationTest
    get '/register'
    assert_response :success
   end
+
+  test "that a profile page works" do
+    get '/profile/Profiler'
+    assert_response :success
+  end
 end
