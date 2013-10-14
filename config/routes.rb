c2b6e83ed78e65fb321a84e625beb475b02d0c74
Matteo Canu <matteo.canu@devise.it>
2013-10-14 17:53:52 +0200

    Added a route to handle /profile/<profile name>

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Treebook::Application.routes.draw do
 
   root :to => "statuses#index"
 
+  get '/profile/:id', to: 'profiles#show'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
