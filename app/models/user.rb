697b32214f88d87ecaf978852ec6d057b51544e9
Matteo Canu <matteo.canu@devise.it>
2013-10-14 09:43:24 +0200

    Added tests for first_name, last_name and profile_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,10 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :profile_name
-  # attr_accessible :title, :body
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :profile_name, presence: true
 
   has_many :statuses
 
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "a user should enter a first name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:first_name].empty?
+  end
+
+  test "a user should enter a last name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:last_name].empty?
+  end
+
+  test "a user should enter a profile name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+  end
 end
