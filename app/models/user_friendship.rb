8144c908d5c48dc7902844b3f41f3461a4eeb15b
Matteo Canu <matteo.canu@devise.it>
2013-10-15 14:54:19 +0200

    Added new accessible attributes

--- app/models/user_friendship.rb
+++ app/models/user_friendship.rb
@@ -2,5 +2,5 @@ class UserFriendship < ActiveRecord::Base
   belongs_to :user
   belongs_to :friend, class_name: 'User', foreign_key: 'friend_id'
 
-  attr_accessible :user, :friend
+  attr_accessible :user, :friend, :user_id, :friend_id
 end
