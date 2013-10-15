6ec7cc01430e5253db74197e151c0d8b97b32fa3
Matteo Canu <matteo.canu@devise.it>
2013-10-15 10:36:45 +0200

    Added helper to generate gravatar url for the profile avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,4 +22,12 @@ class User < ActiveRecord::Base
   def full_name
     first_name + " " + last_name
   end
+
+  def gravatar_url
+    stripped_email = email.strip
+    lowercased_email = stripped_email.downcase
+    hash = Digest::MD5.hexdigest(lowercased_email)
+
+    "http://gravatar.com/avatar/#{hash}"
+  end
 end
