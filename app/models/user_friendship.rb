3bbe644a9410aa397ca24a78751eb84b1021c766
Matteo Canu <matteo.canu@devise.it>
2013-10-15 12:46:43 +0200

    Generated UserFriendship model, defined a migration for the new table

---
+++ app/models/user_friendship.rb
@@ -0,0 +1,3 @@
+class UserFriendship < ActiveRecord::Base
+  # attr_accessible :title, :body
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131011130318) do
+ActiveRecord::Schema.define(:version => 20131015104247) do
 
   create_table "statuses", :force => true do |t|
     t.text     "content"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(:version => 20131011130318) do
   end
 
   add_index "statuses", ["user_id"], :name => "index_statuses_on_user_id"
+
+  create_table "user_friendships", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "user_friendships", ["user_id", "friend_id"], :name => "index_user_friendships_on_user_id_and_friend_id"
 
   create_table "users", :force => true do |t|
     t.string   "first_name"
