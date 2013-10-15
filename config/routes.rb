f826ee45cef9f4eb6f64dade78f2206836443640
Matteo Canu <matteo.canu@devise.it>
2013-10-15 17:48:20 +0200

    Added new routes for the reource :user_friendships and removed some empty lines

--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,10 @@ Treebook::Application.routes.draw do
     get 'logout', to: 'devise/sessions#destroy', as: :logout
   end
 
+  resources :user_friendships
+
   resources :statuses
-
   get 'feed', to: 'statuses#index', as: :feed
-
   root :to => "statuses#index"
 
   get '/profile/:id', to: 'profiles#show', as: :profile_page
