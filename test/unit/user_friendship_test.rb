d22b1737b98ecfc62820daea8b815755176dee25
Matteo Canu <matteo.canu@devise.it>
2013-10-15 14:55:23 +0200

    Added new tests for the friendship functionality

--- test/unit/user_friendship_test.rb
+++ test/unit/user_friendship_test.rb
@@ -8,6 +8,10 @@ class UserFriendshipTest < ActiveSupport::TestCase
     assert_nothing_raised do
      UserFriendship.create user: users(:profiler), friend: users(:moreuser)
     end
+  end
 
+  test "that creating a friendship based on a user id and friend id works" do
+    UserFriendship.create user_id: users(:profiler).id, friend_id: users(:newuser).id
+    assert users(:profiler).friends.include?(users(:newuser))
   end
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+
+  should have_many(:user_friendships)
+  should have_many(:friends)
+
   test "a user should enter a first name" do
     user = User.new
     assert !user.save
@@ -44,4 +48,17 @@ class UserTest < ActiveSupport::TestCase
 
     assert user.valid?
   end
+
+  test "that no error is raised when trying to access a friend list" do
+    assert_nothing_raised do
+      users(:profiler).friends
+    end
+  end
+
+  test "that creating friendships on a user works" do
+    users(:profiler).friends << users(:newuser)
+    users(:profiler).friends.reload
+    assert users(:profiler).friends.include?(users(:newuser))
+  end
+
 end
