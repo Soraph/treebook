40a115756f19fefddebfcd80f79aeb413f41a8fe
Matteo Canu <matteo.canu@devise.it>
2013-10-15 14:27:22 +0200

    Created the test for the UserFriendship model

--- test/unit/user_friendship_test.rb
+++ test/unit/user_friendship_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserFriendshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should belong_to(:user)
+  should belong_to(:friend)
+
+  test "that creating a friendship works without raising an exception" do
+    assert_nothing_raised do
+     UserFriendship.create user: users(:profiler), friend: users(:moreuser)
+    end
+
+  end
 end
