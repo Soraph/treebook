656a8a01f8fec8d2fce782ab579fac32f9370725
Matteo Canu <matteo.canu@devise.it>
2013-10-16 12:49:08 +0200

    Added test for the state and email features

--- test/unit/user_friendship_test.rb
+++ test/unit/user_friendship_test.rb
@@ -14,4 +14,26 @@ class UserFriendshipTest < ActiveSupport::TestCase
     UserFriendship.create user_id: users(:profiler).id, friend_id: users(:newuser).id
     assert users(:profiler).friends.include?(users(:newuser))
   end
+
+  context "a new instance" do
+    setup do
+      @user_friendship = UserFriendship.new user: users(:profiler), friend: users(:newuser)
+    end
+
+    should "have a pending state" do
+      assert_equal 'pending', @user_friendship.state
+    end
+  end
+
+  context "#send_request_email" do
+    setup do
+      @user_friendship = UserFriendship.create user: users(:profiler), friend: users(:newuser)
+    end
+
+    should "send an email" do
+      assert_difference 'ActionMailer::Base.deliveries.size' do
+        @user_friendship.send_request_email
+      end
+    end
+  end
 end
