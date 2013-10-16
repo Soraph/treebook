744bf3544f9f71b61e77d75a4522757fa66630b4
Matteo Canu <matteo.canu@devise.it>
2013-10-16 17:21:58 +0200

    Added friend_request_accepted notification

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -10,4 +10,14 @@ class UserNotifier < ActionMailer::Base
     mail to: @friend.email,
          subject: "#{@friend.first_name} wants to be your friend on Treebook"
   end
+
+  def friend_request_accepted(user_friendship_id) 
+    user_friendship = UserFriendship.find(user_friendship_id)
+
+    @user = user_friendship.user
+    @friend = user_friendship.friend
+
+    mail to: @friend.email,
+         subject: "#{@friend.first_name} has accepted your friend request."
+  end
 end
