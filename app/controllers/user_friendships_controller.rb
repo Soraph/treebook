dbd7f04a265510811adbdaabd1259b0db0ff8082
Matteo Canu <matteo.canu@devise.it>
2013-10-16 11:20:40 +0200

    Created the create method and added a check for an invalid friend_id

--- app/controllers/user_friendships_controller.rb
+++ app/controllers/user_friendships_controller.rb
@@ -3,7 +3,8 @@ class UserFriendshipsController < ApplicationController
 
   def new
     if params[:friend_id]
-      @friend = User.find(params[:friend_id])
+      @friend = User.where(profile_name: params[:friend_id]).first
+      raise ActiveRecord::RecordNotFound if @friend.nil?
       @user_friendship = current_user.user_friendships.new(friend: @friend)
     else
       flash[:error] = "Friend required"
@@ -12,4 +13,20 @@ class UserFriendshipsController < ApplicationController
     render file: 'public/404', status: :not_found
     
   end
+
+  def create
+    if params[:user_friendship] && params[:user_friendship].has_key?(:friend_id)
+      @friend = User.where(profile_name: params[:user_friendship][:friend_id]).first
+      @user_friendship = current_user.user_friendships.new(friend: @friend)
+
+      @user_friendship.save
+      flash[:success] = "You are now friend with #{@friend.full_name}"
+      redirect_to profile_page_path(@friend)
+    else
+      flash[:error] = "Friend required"
+      redirect_to root_path
+    end
+
+  end
+
 end
