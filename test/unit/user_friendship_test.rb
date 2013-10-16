4edfed6ab42171d0597b7bc1b515588092036156
Matteo Canu <matteo.canu@devise.it>
2013-10-16 17:20:32 +0200

    Added test to check the friendship states

--- test/unit/user_friendship_test.rb
+++ test/unit/user_friendship_test.rb
@@ -12,7 +12,7 @@ class UserFriendshipTest < ActiveSupport::TestCase
 
   test "that creating a friendship based on a user id and friend id works" do
     UserFriendship.create user_id: users(:profiler).id, friend_id: users(:newuser).id
-    assert users(:profiler).friends.include?(users(:newuser))
+    assert users(:profiler).pending_friends.include?(users(:newuser))
   end
 
   context "a new instance" do
@@ -36,4 +36,42 @@ class UserFriendshipTest < ActiveSupport::TestCase
       end
     end
   end
+
+  context "#accept!" do
+    setup do
+      @user_friendship = UserFriendship.create user: users(:profiler), friend: users(:newuser)
+    end
+
+    should "set the state to accepted" do
+      @user_friendship.accept!
+      assert_equal "accepted", @user_friendship.state
+    end
+
+    should "send an acceptance email" do
+      assert_difference 'ActionMailer::Base.deliveries.size' do
+        @user_friendship.accept!
+      end
+    end
+
+    should "include the friend in the list of friends" do
+      @user_friendship.accept!
+      users(:profiler).friends.reload
+      assert users(:profiler).friends.include?(users(:newuser))
+    end
+  end
+
+  context ".request" do
+    should "create two user friendships" do
+      assert_difference 'UserFriendship.count', 2 do
+        UserFriendship.request(users(:profiler), users(:newuser))
+      end
+    end
+
+    should "send a friend request email" do
+      assert_difference 'ActionMailer::Base.deliveries.size' do
+        UserFriendship.request(users(:profiler), users(:newuser))
+      end
+    end
+  end
+
 end
