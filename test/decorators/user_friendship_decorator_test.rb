4834c4ab564303efad4aaa1612b43e9bb2858c89
Matteo Canu <matteo.canu@devise.it>
2013-10-18 12:00:52 +0200

    Modified the decorator sub_message and related test

--- test/decorators/user_friendship_decorator_test.rb
+++ test/decorators/user_friendship_decorator_test.rb
@@ -14,7 +14,7 @@ class UserFriendshipDecoratorTest < ActiveSupport::TestCase
       end
 
       should "return the correct message" do
-        assert_equal "Do you really want to be friends with #{@friend.full_name}?", @decorator.sub_message
+        assert_equal "Friend request pending.", @decorator.sub_message
       end
     end
 
