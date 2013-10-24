cd003e674e6b7c1c531c7a2721823909b8b0b35d
Matteo Canu <matteo.canu@devise.it>
2013-10-24 11:48:07 +0200

    Reversed the order of the statuses collection

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -5,7 +5,7 @@ class StatusesController < ApplicationController
   # GET /statuses
   # GET /statuses.json
   def index
-    @statuses = Status.all
+    @statuses = Status.all.reverse_order
 
     respond_to do |format|
       format.html # index.html.erb
