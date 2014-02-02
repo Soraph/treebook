2829aacb16953f95dfbdbceca72eb8a87d042adb
Soraph <soraph88@gmail.com>
2014-02-02 10:57:44 +0100

    Added devise parameter sanitizerto comply to the updated gems

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:first_name, :last_name, :profile_name, :email, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(:first_name, :last_name, :profile_name)
+    end
+  end
 end
