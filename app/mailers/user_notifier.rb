30777297421e653b7870d15dc934c3f6a7b9816b
Soraph <soraph88@gmail.com>
2014-02-04 18:40:30 +0100

    Simplified the UserNotifier, added friend_notification

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -1,23 +1,21 @@
 class UserNotifier < ActionMailer::Base
   default from: "from@example.com"
 
-  def friend_requested(user_friendship_id) 
-    user_friendship = UserFriendship.find(user_friendship_id)
-
-    @user = user_friendship.user
-    @friend = user_friendship.friend
-
-    mail to: @friend.email,
-         subject: "#{@friend.first_name} wants to be your friend on Treebook"
+  def friend_requested(user_friendship_id)
+    friend_notification(user_friendship_id,"wants to be your friend on Treebook")
   end
 
   def friend_request_accepted(user_friendship_id) 
+    friend_notification(user_friendship_id,"has accepted your friend request")
+  end
+
+  def friend_notification(user_friendship_id, email_subject)
     user_friendship = UserFriendship.find(user_friendship_id)
 
     @user = user_friendship.user
     @friend = user_friendship.friend
 
     mail to: @friend.email,
-         subject: "#{@friend.first_name} has accepted your friend request."
+         subject: "#{@friend.first_name} #{email_subject}."
   end
 end
